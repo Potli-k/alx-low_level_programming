150eb9423becb1c41ef89d1ad748186ec4499843
Potli-k <medupepotlako@gmail.com>
2023-03-31 01:57:34 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
 #include "main.h"
+
 /**
- * main - prints the string from a character array.
- * Return: 0 on success.
+ * main - Prints followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char c; 
-
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
